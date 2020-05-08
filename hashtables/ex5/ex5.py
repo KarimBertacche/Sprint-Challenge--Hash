9eb5e92fd0a1a129f18fdaf615ffa0f2fe067854
Karim B <mattiakarim@live.it>
2020-05-08 17:13:35 +0200

    Complete challenge 5 implementation to match path from queries

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -6,15 +6,16 @@ def finder(files, queries):
     files_dict = dict()
     result = list()
 
+    for q in queries:
+        files_dict[q] = list()
+
     for idx, file in enumerate(files):
-        for q in queries:
-            if q in files[idx]:
-               files_dict[idx] = file 
-
-    for idx in files_dict:
-        result.append(files[idx])
-
-    print(result)
+        path = file.split('/')[-1]
+        if path in files_dict:
+            files_dict[path].append(file)
+            result.append(file)
+        else:
+            continue
 
     return result
 
@@ -31,16 +32,3 @@ if __name__ == "__main__":
         "baz"
     ]
     print(finder(files, queries))
-
-# finder(files, queries)
-
-# first solution, very slow for very large file size
-
-# for q in queries:
-#     for idx, file in enumerate(files):
-#         path = file.split('/')[1:]
-#         if q in path:
-#             files_dict[idx] = file
-
-# for idx in files_dict:
-#     result.append(files[idx])
